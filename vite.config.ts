4144de719596f162899a60beaf0c5af9fc00dc15
mtra <jeff.j.lu2@gmail.com>
2023-09-02 03:16:46 +1000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Anarchy-Forum/",
   plugins: [react()],
-  base: "/Anarchy-Forum",
 })
